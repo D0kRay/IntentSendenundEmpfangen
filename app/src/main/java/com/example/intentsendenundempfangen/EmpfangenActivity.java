3e411d5e13f52688b0faf55b4d063db54ce0927e
D0kRay <50095938+D0kRay@users.noreply.github.com>
2022-10-13 09:33:33 +0200

    Initial commit

---
+++ app/src/main/java/com/example/intentsendenundempfangen/EmpfangenActivity.java
@@ -0,0 +1,19 @@
+package com.example.intentsendenundempfangen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class EmpfangenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_empfangen);
+    }
+
+    public void onClickEmpfangenSenden(View view) {
+
+    }
+}
